27425114ce83721265193b6fc634c737886618f2
Kris <131913157+Kludwisz@users.noreply.github.com>
2025-04-18 15:52:25 +0000

    feat(panopale): set up kernel input, make fpops bound bigger

--- images/makeproject/project/bin/workgen/panopale-workgen.sh
+++ images/makeproject/project/bin/workgen/panopale-workgen.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#prepare input files for use by the app
+for file in ./panopale_configs/*;
+do
+    echo "Staging file $file"
+    bin/stage_file --verbose --copy "$file"
+done
+
 #command_line has the --device flag to trick the wrapper into passing args to the child app
 for i in {0..262144}; do
 wu_name="panopale_1.00_$i"
@@ -7,7 +14,7 @@ wu_name="panopale_1.00_$i"
   bin/create_work --appname panopale \
     --wu_template templates/panopale_in \
     --result_template templates/panopale_out \
-    --command_line "--device \"0 --start $((4 * i)) --end $(((4 * i) + 1))\"" \
+    --command_line "--start $((4 * i)) --end $(((4 * i) + 1))" \
     --wu_name "${wu_name}" \
     --min_quorum 2 \
     --credit 2500
