a303886ec092f2264574c60ce16b4013e465441e
Kris <131913157+Kludwisz@users.noreply.github.com>
2025-04-18 22:14:13 +0200

    feat(panopale): Add panopale project
    * feat(project): add workgen and io templates for panopale project
    
    * feat(project): add dummmy version.xml files for panopale
    
    * feat(project): add dummmy version.xml files for panopale
    
    * feat(project): add assimilator and validator for panopale
    
    * feat(project): add assimilator and validator for panopale
    
    * feat(project): add kernel input to panopale input template
    
    * feat(project): Add panopale executables
    
    * feat(project): Add windows executable for panopale
    
    * feat(project): update assimilator and workgen script for panopale
    
    * fix: remove temporary todo file
    
    * feat(project): add panopale app in project.xml
    
    * feat(panopale): set up kernel input, make fpops bound bigger
    
    * fix(panopale): remove outdated comment
    
    * fix(panopale): update linux executable name to make it pass integration tests
    
    * fix(panopale): *actually* update linux executable name to make it pass integration tests
    
    * fix(panopale): rename executables to match project versions
    
    * fix(project): fix xml structure in project.xml
    
    * fix(panopale): add execute permission to workgen and assimilator
    
    * fix(ci): Fix error message for failed permissions check

---
+++ images/makeproject/project/bin/workgen/panopale-workgen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#prepare input files for use by the app
+for file in ./panopale_configs/*;
+do
+    echo "Staging file $file"
+    bin/stage_file --verbose --copy "$file"
+done
+
+for i in {0..262144}; do
+wu_name="panopale_1.00_$i"
+  echo "create_work: ${wu_name}"
+  bin/create_work --appname panopale \
+    --wu_template templates/panopale_in \
+    --result_template templates/panopale_out \
+    --command_line "--start $((4 * i)) --end $(((4 * i) + 1))" \
+    --wu_name "${wu_name}" \
+    --min_quorum 2 \
+    --credit 2500
+done
