90162e36322c6d342af0af3f7e90fbf1517c8c4f
Kris <131913157+Kludwisz@users.noreply.github.com>
2025-04-20 11:46:01 +0200

    fix(panopale): v1.01, Fix workgen script and bad invalid arg logging, make the tasks 64x bigger
    * fix(panopale): Fix workgen script and bad invalid arg logging, bump app version to 1.01
    
    * fix(panopale): fix linux executable name
    
    * fix(panopale): Make the tasks 64x bigger

--- images/makeproject/project/bin/workgen/panopale-workgen.sh
+++ images/makeproject/project/bin/workgen/panopale-workgen.sh
@@ -7,13 +7,13 @@ do
     bin/stage_file --verbose --copy "$file"
 done
 
-for i in {0..262144}; do
-wu_name="panopale_1.00_$i"
+for i in {0..4096}; do
+wu_name="panopale_1.01_$i"
   echo "create_work: ${wu_name}"
   bin/create_work --appname panopale \
     --wu_template templates/panopale_in \
     --result_template templates/panopale_out \
-    --command_line "--start $((4 * i)) --end $(((4 * i) + 1))" \
+    --command_line "--start $((256 * i)) --end $((256 * (i + 1)))" \
     --wu_name "${wu_name}" \
     --min_quorum 2 \
     --credit 2500
