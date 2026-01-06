67efecc98efddb816ccb96f5e2628aa215667f14
Kris <131913157+Kludwisz@users.noreply.github.com>
2026-01-06 20:51:20 +0000

    update workgen script with real data

--- images/makeproject/project/bin/workgen/diaveins-workgen.sh
+++ images/makeproject/project/bin/workgen/diaveins-workgen.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# WARNING! This is not ready to run!
-
-for i in {0..262143}; do
+for i in {0..65536}; do
 wu_name="diaveins_1.00_$i"
   echo "create_work: ${wu_name}"
   bin/create_work --appname diaveins \
     --wu_template templates/diaveins_in \
     --result_template templates/diaveins_out \
-    --command_line "--start $((i * 1024)) --end $(((i + 1) * 1024))" \
+    --command_line "--start $((i * 4096)) --end $(((i + 1) * 4096))" \
     --wu_name "${wu_name}" \
     --min_quorum 2 \
-    --credit 50000
+    --credit 30000
 
 done
